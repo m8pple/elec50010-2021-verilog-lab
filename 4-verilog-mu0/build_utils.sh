985b7c4ff6b7d3d19fd1d876c3a0e9ca8bbd5c3e
m8pple <dt10@imperial.ac.uk>
2021-10-29 09:51:28 +0100

    Added missing "mkdir bin". Thanks to Yann.

--- 4-verilog-mu0/build_utils.sh
+++ 4-verilog-mu0/build_utils.sh
@@ -5,6 +5,7 @@ set -eou pipefail
 MU0_SRCS="utils/mu0_assembly.cpp utils/mu0_disassembly.cpp utils/mu0_simulate.cpp"
 
 echo "Building MU0 utils" > /dev/stderr
+mkdir -p bin
 g++ -o bin/assembler utils/assembler.cpp ${MU0_SRCS}
 g++ -o bin/simulator utils/simulator.cpp ${MU0_SRCS}
 g++ -o bin/disassembler utils/disassembler.cpp ${MU0_SRCS}
